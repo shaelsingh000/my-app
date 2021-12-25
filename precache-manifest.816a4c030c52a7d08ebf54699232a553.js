ca679bc531835de9eb05ea7a6853a6e045bebcd4
shaelsingh000 <64468587+shaelsingh000@users.noreply.github.com>
2021-12-25 20:05:39 +0530

    Updates

---
+++ precache-manifest.816a4c030c52a7d08ebf54699232a553.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "48bfcca9142c16ebd4c0",
+    "url": "git@github.com:shaelsingh000/my-app.git/static/css/main.2a69ec44.chunk.css"
+  },
+  {
+    "revision": "48bfcca9142c16ebd4c0",
+    "url": "git@github.com:shaelsingh000/my-app.git/static/js/main.48bfcca9.chunk.js"
+  },
+  {
+    "revision": "e88f3e01182016b2c5ca",
+    "url": "git@github.com:shaelsingh000/my-app.git/static/js/1.e88f3e01.chunk.js"
+  },
+  {
+    "revision": "081792daa85d618e997e",
+    "url": "git@github.com:shaelsingh000/my-app.git/static/css/2.f0a9a989.chunk.css"
+  },
+  {
+    "revision": "081792daa85d618e997e",
+    "url": "git@github.com:shaelsingh000/my-app.git/static/js/2.081792da.chunk.js"
+  },
+  {
+    "revision": "0b39b850133dab817931",
+    "url": "git@github.com:shaelsingh000/my-app.git/static/js/runtime~main.0b39b850.js"
+  },
+  {
+    "revision": "82a29430f8beacc45f23c43ca9d768a5",
+    "url": "git@github.com:shaelsingh000/my-app.git/index.html"
+  }
+];
